f6d336a267e6334f9f52660f69941afb883458f1
martinguti95 <martinguti95@gmail.com>
2020-09-05 12:32:42 +0200

    Included connection to PROD database

--- dal/index.js
+++ dal/index.js
@@ -1,13 +1,17 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const connectionOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-}
+};
 
-const connectDB = () => mongoose.connect(
-  process.env.DATABASE_URL_DEV || 'mongodb://localhost:27017/rduckr', connectionOptions,
-)
+const connectDB = () =>
+  mongoose.connect(
+    process.env.DATABASE_URL_PROD ||
+      process.env.DATABASE_URL_DEV ||
+      "mongodb://localhost:27017/rduckr",
+    connectionOptions
+  );
 
-export { connectDB }
+export { connectDB };
