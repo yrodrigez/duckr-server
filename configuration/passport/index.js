6dd131fb2d05af4367395c7df8f506ea5e5f6da5
martinguti95 <martinguti95@gmail.com>
2021-01-28 20:12:30 +0100

    Added Google strategy and new connection to remote database

--- configuration/passport/index.js
+++ configuration/passport/index.js
@@ -1,29 +1,30 @@
-import LocalStrategy from './strategies/LocalStrategy'
+import LocalStrategy from "./strategies/LocalStrategy";
 
-
-import passport from 'passport'
-import jwt from 'jsonwebtoken'
-import JwtStrategy from './strategies/JwtStrategy'
-import FacebookStrategy from './strategies/FacebookStrategy'
-
+import passport from "passport";
+import jwt from "jsonwebtoken";
+import JwtStrategy from "./strategies/JwtStrategy";
+import FacebookStrategy from "./strategies/FacebookStrategy";
+import GoogleStrategy from "./strategies/GoogleStrategy";
 
 const LOGIN_STRATEGY = {
-  LOCAL: 'local',
-  JWT: 'jwt',
-  FACEBOOK: 'facebook',
-}
+  LOCAL: "local",
+  JWT: "jwt",
+  FACEBOOK: "facebook",
+  GOOGLE: "google",
+};
 
-passport.use( LOGIN_STRATEGY.JWT, JwtStrategy )
+passport.use(LOGIN_STRATEGY.JWT, JwtStrategy);
 
-passport.use( LOGIN_STRATEGY.LOCAL, LocalStrategy )
-passport.use( LOGIN_STRATEGY.FACEBOOK, FacebookStrategy )
+passport.use(LOGIN_STRATEGY.LOCAL, LocalStrategy);
+passport.use(LOGIN_STRATEGY.FACEBOOK, FacebookStrategy);
+passport.use(LOGIN_STRATEGY.GOOGLE, GoogleStrategy);
 
-passport.serializeUser( ( user, done ) => {
-  done( null, user )
-} )
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
 
-passport.deserializeUser( ( user, done ) => {
-  done( null, user )
-} )
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
-export { passport, LOGIN_STRATEGY }
+export { passport, LOGIN_STRATEGY };
